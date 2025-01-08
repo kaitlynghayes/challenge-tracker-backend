42956919beb98a8339bcbcdc61ab4066e57f6dbc
Kaitlyn Hayes <khayes@ara.com>
2025-01-08 13:34:01 -0600

    Added properties and changed project name
    - Added Challenge and ChallengeLength properties
    - Changed project name from challenge-tracker-backend to challenge-tracker-data-access

---
+++ challenge-tracker-backend/challenge-tracker-backend/Entities/Challenge.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace challenge_tracker_backend.Entities
+{
+  public class Challenge
+  {
+    public Guid Id { get; set; }
+    [Required]
+    public string Name { get; set; } = string.Empty;
+    [Required]
+    public string Description { get; set; } = string.Empty;
+    public DateTime UpdatedTime { get; set; }
+    public DateTime CreatedTime { get; set; }
+    public DateTime CompletedTime { get; set; }
+    public bool IsCompleted { get; set; }
+
+    // Foreign key to ChallengeLength (1 to 1)
+    public Guid ChallengeLengthId { get; set; }
+    public ChallengeLength ChallengeLength { get; set; }
+  }
+}
---
+++ challenge-tracker-backend/challenge-tracker-backend/Entities/ChallengeLength.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace challenge_tracker_backend.Entities
+{
+  public class ChallengeLength
+  {
+    public Guid Id { get; set; }
+
+    // Start and end dates of the challenge
+    [Required]
+    public DateTime ChallengeStartDate { get; set; }
+    [Required]
+    public DateTime ChallengeEndDate { get; set; }
+
+    // Duration properties (calculated from start and end dates)
+    public TimeSpan Duration  => ChallengeStartDate - ChallengeEndDate;
+    public int Days => Duration.Days;
+    public int Weeks => (int)(Duration.TotalDays / 7);
+    public int Years => (int)(Duration.TotalDays / 365);
+
+    // Foreign key to Challange (1 to 1)
+    public Guid ChallengeId { get; set; }
+    public Challenge Challenge { get; set; }
+  }
+}
